6d86f15c81a196a32290b41f2dd0d54b58c471f7
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-01-05 23:25:53 +0600

    Add DNA matching logic in dna.py
    Implement DNA matching functionality by reading a database and a DNA sequence file, and finding the longest match for each subsequence.

---
+++ Week 6 - Python/dna/dna.py
@@ -0,0 +1,75 @@
+import csv
+import sys
+
+
+def main():
+  
+    if len(sys.argv) != 3:
+        sys.exit("python dna.py data.csv sequence.txt")
+
+    # TODO: Read database file into a variable
+    database = []
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
+        for raw in reader:
+            database.append(raw)
+
+
+    with open(sys.argv[2], "r") as file:
+        dna_sequence = file.read()
+
+  
+    subsequences = list(database[0].keys())[1:]
+    result = {}
+    for subsequence in subsequences:
+        result[subsequence] = longest_match(dna_sequence, subsequence)
+
+   
+    for person in database:
+        match = 0
+        for subsequence in subsequences:
+            if int(person[subsequence]) == result[subsequence]:
+                match += 1
+
+        if match == len(subsequences):
+            print(person["name"])
+            return
+    print("No match")
+
+    return
+
+
+def longest_match(sequence, subsequence):
+    """Returns length of longest run of subsequence in sequence."""
+
+  
+    longest_run = 0
+    subsequence_length = len(subsequence)
+    sequence_length = len(sequence)
+
+    for i in range(sequence_length):
+        
+        count = 0
+
+       
+        while True:
+        
+            start = i + count * subsequence_length
+            end = start + subsequence_length
+
+         
+            if sequence[start:end] == subsequence:
+                count += 1
+
+     
+            else:
+                break
+
+        
+        longest_run = max(longest_run, count)
+
+    
+    return longest_run
+
+
+main()
